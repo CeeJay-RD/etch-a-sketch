c72d5d763387edf312a1ff2dd38d031863ae8e71
KamajiRD <adamescarlos2808@gmail.com>
2022-12-14 21:04:20 -0400

    Added color on hover

--- script.js
+++ script.js
@@ -6,8 +6,21 @@ function makeRows(rows, cols) {
     for (c = 0; c < (rows * cols); c++) {
       let cell = document.createElement("div");
       cell.innerText = (c + 1);
+      cell.addEventListener('mouseover', changeGridColor)
       gridContainer.appendChild(cell).className = "grid-item";
     };
   };
 
   makeRows(16, 16);
+
+function changeGridColor(e) {
+  let color = `rgb(${[0, 0, 0].map(channel => {
+    return Math.floor(Math.random() * 256);
+  }).join(',')})`
+  Object.assign(e.target.style, {
+    backgroundColor : color,
+    opacity : 0.25
+  });
+}
+
+  
